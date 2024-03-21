e13f4574b4a888f43f840fb603efbbe382333c95
Thapa0 <rakeshmagarr0@gmail.com>
2024-03-21 17:36:20 +0530

    update

--- src/utils/connectDb.js
+++ src/utils/connectDb.js
@@ -1,11 +1,17 @@
 import { createPool } from "mysql2/promise";
 
+const host = process.env.HOST || "127.0.0.1"
+const port = process.env.SQL_PORT || 3306
+const user = process.env.USER || "root"
+const password = process.env.PASSWORD || "root"
+const database = process.env.DATABASE
+
 const connectDb = createPool({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "assignments"
+    host,
+    port,
+    user,
+    password,
+    database
 });
 
 export default connectDb;
